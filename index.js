00a97f421417df8c893b60b1214197926b97c901
zydevstudios <robloxbestgamer111@gmail.com>
2026-04-22 21:38:23 +0100

    Initialize Express server in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello from my Linux Server!'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
